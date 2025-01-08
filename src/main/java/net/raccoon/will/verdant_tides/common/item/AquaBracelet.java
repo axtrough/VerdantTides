094211eea78a9903129165d82ab36c3c2e8eed54
axtrough <lolmyfriend14@gmail.com>
2025-01-08 03:50:16 +0100

    smooth dash. code looks terrible but it works 👍

--- src/main/java/net/raccoon/will/verdant_tides/common/item/AquaBracelet.java
+++ src/main/java/net/raccoon/will/verdant_tides/common/item/AquaBracelet.java
@@ -3,6 +3,7 @@ package net.raccoon.will.verdant_tides.common.item;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -20,36 +21,37 @@ public class AquaBracelet extends Item implements ICurioItem {
     public void curioTick(SlotContext slotContext, ItemStack stack) {
         Player player = (Player) slotContext.entity();
         CompoundTag data = player.getPersistentData();
-
-        boolean isNowInWater = player.isEyeInFluidType(Fluids.WATER.getFluidType());
-        boolean hasDashed = data.getBoolean("HasDashed");
-        boolean isCrouching = player.isCrouching();
+        boolean hasDashed = data.getBoolean("hasDashed");
         boolean isSwimming = player.isSwimming();
 
+        boolean isNowInWater = player.isInWaterOrBubble();
         if (isNowInWater) {
             Vec3 movement = isSwimming ? player.getLookAngle().scale(0.03) : player.getKnownMovement().scale(0.15);
             player.addDeltaMovement(movement);
-        }
-        else if (!isNowInWater && !hasDashed && !isCrouching) {
-            double dashStrength = 0.75;
-            Vec3 lookDirection = player.getLookAngle();
-            Vec3 dashVelocity = lookDirection.scale(dashStrength).multiply(1.1, 1.0, 1.1);
-            player.addDeltaMovement(dashVelocity);
-            data.putBoolean("HasDashed", true);
-        }
-        if (isNowInWater && hasDashed) {
-            data.putBoolean("HasDashed", false);
-        }
-
-        if (isNowInWater) {
             player.addEffect(new MobEffectInstance(MobEffects.WATER_BREATHING, 300, 0, false, false));
         } else {
             player.removeEffectNoUpdate(MobEffects.WATER_BREATHING);
         }
 
-        ICurioItem.super.curioTick(slotContext, stack);
+        if (!isNowInWater && !hasDashed) {
+            double dashStrength = 0.75;
+            Vec3 lookDirection = player.getLookAngle();
+            Vec3 dashVelocity = lookDirection.scale(dashStrength).multiply(1.2, 1.2, 1.2);
+            player.addDeltaMovement(dashVelocity);
+            player.setForcedPose(Pose.FALL_FLYING);
+            player.getDeltaMovement().multiply(0.5,0.5,0.5);
+            data.putBoolean("hasDashed", true);
+        }
+
+        if (hasDashed && isNowInWater) {
+            data.putBoolean("hasDashed", false);
+        }else if (hasDashed && player.onGround())
+            player.setForcedPose(null);
+
+            ICurioItem.super.curioTick(slotContext, stack);
     }
 }
+
 
 
 
